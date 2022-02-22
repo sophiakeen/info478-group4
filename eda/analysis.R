03873236d890a6c09f236ebc76aeb82728419e69
kvpoch <kathypoch11@gmail.com>
2022-02-21 23:42:22 -0800

    added bar plot

--- eda/analysis.R
+++ eda/analysis.R
@@ -2,6 +2,7 @@
 library(dplyr)
 library(ggplot2)
 library(tidyverse)
+library(plotly)
 
 # Load data
 adult_depression <- read.csv('data/adult-depression-lghc-indicator-24.csv')
@@ -47,3 +48,31 @@ ggplot(data = mental_hospital_rates, aes(x = suicide_rates,
                                                      y = mental_hospitals)) + geom_point(shape = 1) +
   labs(x = "Suicide Rates", y = "Mental Hospitals",
        title = "Number of Mental Hospitals vs Suicide Rates")
+
+
+#How does socioeconomic status level impact prevalence of depression? 
+
+ad_2018 <- adult_depression %>% 
+  filter(Year == 2018)
+ad_2018_wider <- ad_2018 %>% pivot_wider(names_from = Strata,
+                                          values_from = c(Strata.Name, Frequency, Weighted.Frequency,
+                                                          Lower.95..CL, Upper.95..CL))
+
+ad_2018_income <- ad_2018_wider %>% select('Year', 'Strata.Name_Income', 'Frequency_Income',
+                                           'Weighted.Frequency_Income', 'Lower.95..CL_Income',
+                                           'Upper.95..CL_Income')
+ad_2018_income <- na.omit(ad_2018_income)
+
+
+ad_2018_income_plot <- ggplot() + geom_col(data = ad_2018_income, 
+                                           aes(x = Strata.Name_Income, y = Weighted.Frequency_Income, fill = "#C06C84")) + 
+  coord_flip() +
+  labs(x = "Income", y = "Weighted Frequency", 
+       title = "2018 Income vs. Weighted Frequency of Adult Depression (CA)") +
+  theme(legend.position = "none")
+
+ad_2018_income_plotly <- ggplotly(ad_2018_income_plot)
+
+options(scipen=5) #force no E (scientific notation) in values
+
+
